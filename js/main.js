4fd80a8ff0eb83a9a630e79b7038095121f73bba
Alex Khalaimov <alex0880@rambler.ru>
2018-02-16 11:27:45 +0200

    5

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-    if ($('.stage1bForm').length != 0) {
+    if ($('.stageForm').length != 0) {
         $('input:checkbox').on('change', function(){
            if ($(this).prop('checked')) {
                $(this).parent().addClass('_checked');
@@ -13,4 +13,11 @@ $(function () {
             $(this).parent().addClass('_checked');
         });
     }
+    if ($('.step1aPopup').length !=0) {
+        $('#stage1cModalTrigger').click(function (event) {
+            event.preventDefault();
+            $('#stage1cModal').modal();
+        })
+    }
+    
 })
